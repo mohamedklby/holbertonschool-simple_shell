0d2aba1514d945dc0a287502f2f7266e9fe417e9
aaymenb <9954@holbertonstudents.com>
2025-01-06 14:24:32 +0100

    aymen

--- simpleshell1.c
+++ simpleshell1.c
@@ -1,46 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <string.h>
 /**
  * main - execute the commands from the prompt
  * Return: 0
  */
+#include <stdio.h>
+
 int main(void)
 {
-	int status, i;
-	char *buffer;
-	char *argv[2];
-	char *list = "/bin/ls";
-	pid_t child_pid;
-	size_t size;
-
-	size = 32;
-	buffer = malloc(sizeof(char) * size);
-	if (buffer == NULL)
-		return (-1);
-	argv[0] = buffer;
-	argv[1] = NULL;
-	printf("#cisfun$ ");
-	getline(&buffer, &size, stdin);
-	buffer[strcspn(buffer, "\n")] = 0;
-	child_pid = fork();
-	if (child_pid == 0)
-	{
-		i = 0;
-		while (argv[0][i] == list[i])
-			i++;
-		if (i == strlen(list) + 1)
-			execve(argv[0], argv, NULL);
-
-		else
-			printf("./shell: No such file or directory\n");
-		free(buffer);
-		return (-1);
-	}
-	else
-		wait(&status);
-	free(buffer);
+	printf("Hello, World!\n");
 	return (0);
 }
